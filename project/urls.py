652c19c95a5c24716763d87f87efb2d8f285480e
Saleh <cobecode7@gmail.com>
2021-01-04 02:53:22 +0300

    add nested url & basic view structure'

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add path('', views.home, name='home')a URL to urlpatterns:  
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url , include
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -22,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('post.urls', namespace='blog')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
